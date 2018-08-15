5ac1347b35f905d83022b9ac77ea096b6edd1624
Hugo Almeida <hugo@jedi.local>
2018-08-14 22:14:53 -0300

    fisrt commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <input-tag></input-tag>
+  </div>
+</template>
+
+<script>
+  import InputTag from '@/components/Input-tagging.vue';
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        x: ''
+      }
+    },
+    components:{ InputTag }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Input-tagging.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name:'InputTaggingVue',
+}
+</script>
+
+
+<template>
+    <section>
+
+        <div>
+            <input type="text" placeholder="Informe o Produto">
+        </div>
+
+    </section>
+</template>
